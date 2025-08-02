ed04a08a15279c438cfc8fabe212389d84559df3
Dhruvil316 <dhruvil.developer@gmail.com>
2025-08-02 18:00:43 +0530

    Annotations-no-xml

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,7 @@ package org.example;
 import Entities.Task;
 import dao.TaskDao;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import java.util.List;
 
 //to start server : brew services start mysql
@@ -13,8 +12,8 @@ public class Main {
         System.out.println("Program started");
 
 //      spring jdbc -> jdbcTemplate
-        ApplicationContext context = new ClassPathXmlApplicationContext("jdbc-template/config.xml") ;
-        TaskDao task = context.getBean("taskDaoImpl" , TaskDao.class) ; // we can use the parent
+        ApplicationContext context = new AnnotationConfigApplicationContext(JdbcConfig.class) ;
+        TaskDao task = context.getBean("taskDao" , TaskDao.class) ; // we can use the parent
         List<Task> allTasks = task.getAllTasks() ;
         for ( Task t : allTasks ) {
             System.out.println(t);
